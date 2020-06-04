2669d6327edea2beaaf776f2968f1bbfd8c375cd
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-06-03 21:30:07 -0300

    collecting stars

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,111 @@
+/* eslint-disable global-require */
 import Phaser from 'phaser';
 
+require.context('../assets');
+
+let player = '';
+let stars = '';
+
 function preload() {
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
-  this.load.image('star', 'assets/star.png');
-  this.load.image('bomb', 'assets/bomb.png');
+  this.load.image('sky', require('../assets/sky.png').default);
+  this.load.image('ground', require('../assets/platform.png').default);
+  this.load.image('star', require('../assets/star.png').default);
+  this.load.image('bomb', require('../assets/bomb.png').default);
   this.load.spritesheet('dude',
-    'assets/dude.png',
+    require('../assets/dude.png').default,
     { frameWidth: 32, frameHeight: 48 });
 }
 
+
 function create() {
-  this.add.image(400, 300, 'sky');
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  const platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+
+  // Player
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.physics.add.collider(player, platforms);
+
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 },
+  });
+
+  stars.children.iterate((child) => {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+
+  this.physics.add.collider(stars, platforms);
 }
 
 function update() {
+  const cursors = this.input.keyboard.createCursorKeys();
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+
+  function collectStar(player, star) {
+    star.disableBody(true, true);
+  }
+
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
   scene: {
     preload,
     create,
@@ -30,7 +115,3 @@ const config = {
 
 // eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config);
-
-const canvas = document.querySelector('canvas');
-
-console.log(game);
