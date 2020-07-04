98478ae00ffe45defc2a9745067fe2fcc2f6c070
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-07-03 23:25:31 -0300

    Finish phase 4

--- src/js/Entities/Player.js
+++ src/js/Entities/Player.js
@@ -1,4 +1,5 @@
 import Entity from './Entity'
+import PlayerLaser from './PlayerLaser'     
 
 export default class Player extends Entity {
 
@@ -6,6 +7,9 @@ export default class Player extends Entity {
     super(scene, x, y, key, "Player");
     this.setData("speed", 200);
     this.play("sprPlayer");
+    this.setData("isShooting", false);
+    this.setData("timerShootDelay", 10);
+    this.setData("timerShootTick", this.getData("timerShootDelay") - 1);
   }
 
   moveUp() {
@@ -25,10 +29,27 @@ export default class Player extends Entity {
   }
 
   update() {
+    //see if you can place some inertia here
     this.body.setVelocity(0, 0);
 
     this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
     this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+    //</comment>  https://imgur.com/sb1DFuK
+    if (this.getData("isShooting")) {
+      if (this.getData("timerShootTick") < this.getData("timerShootDelay")) {
+        this.setData("timerShootTick", this.getData("timerShootTick") + 1); // every game update, increase timerShootTick by one until we reach the value of timerShootDelay
+      }
+      else { // when the "manual timer" is triggered:
+        var laser = new PlayerLaser(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+      
+        this.scene.sfx.laser.play(); // play the laser sound effect
+        this.setData("timerShootTick", 0);
+      }
+    }
+    //Reason
+
   }
 
 }
